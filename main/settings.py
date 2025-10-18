09e58f5d0240b6892af49f7914d198abb7a3e9df
Onengiye <onengiyeamadi@gmail.com>
2025-10-18 16:44:37 +0100

    To remove it and return only JSON even in the browser

--- main/settings.py
+++ main/settings.py
@@ -130,3 +130,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+}
